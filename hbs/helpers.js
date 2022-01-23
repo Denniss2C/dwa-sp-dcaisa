a5c4b4635d0243172b950c1295ffbd10edad4f1a
Denniss2C <67333558+Denniss2C@users.noreply.github.com>
2022-01-23 16:13:26 -0500

    arreglo
    fallo de helpers e implementacion de arreglos dinámicos

---
+++ hbs/helpers.js
@@ -0,0 +1,16 @@
+//Helpers
+/*
+const express = require('express');
+var hbs = express();
+//const res =require("espress/lib/response");
+var nombres = ["Pizza", "Hamburguesa", "Momo", "Crep", "Hot Dog", "Alitas"];
+var precios = ["$1.50", "Hamburguesa", "Momo", "Crep", "Hot Dog", "Alitas"];
+
+hbs.get('/views/partials/', (req,res)=>{
+    res.render("home",{
+        nombre : nombres[0],
+        precio : precios[0],
+    });
+});
+
+*/
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 //const res =require("espress/lib/response");
 const app = express();
 const hbs = require('hbs');
+const nombres = ["Pizza", "Hamburguesa", "Momo", "Crep", "Hot Dog", "Alitas"];
+const precios = ["$1.50", "$1.78", "$0.50", "$1.50", "$1.80", "$2.25"];
+const descrip = ["Queso, piña", "Pan, carne", "Arina, carne", "Fresas, crema, helado", "Salchicha, salsas", "BBQ, salteadas"];
 
 require('./hbs/helpers');
 
@@ -13,10 +16,34 @@ app.use(express.static(__dirname+'/public'));
 app.set('view engine', 'hbs');
 
 hbs.registerPartials(__dirname + '/views/partials');
-
+/*
 app.get("/",(req,res)=>{
     res.render("home",{
         nombre:"Juan"
+    });
+});*/
+
+app.get('/', (req,res)=>{
+    res.render("home",{
+        nombre1 : nombres[0],
+        precio1 : precios[0],
+        desc1 : descrip[0],
+        nombre2 : nombres[1],
+        precio2 : precios[1],
+        desc2 : descrip[1],
+        nombre3 : nombres[2],
+        precio3 : precios[2],
+        desc3 : descrip[2],
+        nombre4 : nombres[3],
+        precio4 : precios[3],
+        desc4 : descrip[3],
+        nombre5 : nombres[4],
+        precio5 : precios[4],
+        desc5 : descrip[4],
+        nombre6 : nombres[5],
+        precio6 : precios[5],
+        desc6 : descrip[5],
+
     });
 });
 
@@ -27,6 +54,5 @@ app.get("/about",(req,res)=>{
 app.listen(port,()=>
     console.log("escuchando en el puerto ${port}")
 );
-
 
 
