5c3ffb71480f5de6b2d3c2a2aa3913c15c84f504
Denniss2C <67333558+Denniss2C@users.noreply.github.com>
2022-01-24 15:54:06 -0500

    port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const descrip = ["Queso, piña, jamón", "Pan, carne, papas", "Arina, carne, pol
 
 require('./hbs/helpers');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname+'/public'));
 
